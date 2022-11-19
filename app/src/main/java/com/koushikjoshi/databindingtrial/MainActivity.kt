f875d6dbf63dc9c383f80794e470a7a0c448424e
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2022-11-19 17:07:36 +0530

    Added logic

--- app/src/main/java/com/koushikjoshi/databindingtrial/MainActivity.kt
+++ app/src/main/java/com/koushikjoshi/databindingtrial/MainActivity.kt
@@ -2,12 +2,28 @@ package com.koushikjoshi.databindingtrial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.koushikjoshi.databindingtrial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.submitButton.setOnClickListener {
+
+            displayGreetingText()
+
+        }
+
+
+    }
+
+    private fun displayGreetingText() {
+
+        binding.greetingTextView.text = "Hello " + binding.nameEditText.text
 
     }
 }
